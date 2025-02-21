eb3caf3214389a7fe2e6c32f1b4a3a6b4074864b
Sadia0920 <sadiaafrin0920@gmail.com>
2025-02-21 12:51:27 +0600

    implement delete method in tasks collection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -47,10 +47,31 @@ async function run() {
     })
 
     // tasks
+    app.get('/tasks/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await tasksCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get('/tasks',async(req,res)=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await tasksCollection.find(query).toArray()
+      res.send(result);
+    })
+
     app.post('/tasks',async(req,res)=>{
       const note = req.body;
       const result = await tasksCollection.insertOne(note)
       res.send(result)
+    })
+
+    app.delete('/tasks/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
